58a70846d1d5be8cc6840a9a976c17fa5795cf22
Maciej Dobrowolski <maciej.dobrowolski@unity3d.com>
2023-05-09 13:07:23 -0400

    init commit

---
+++ extract.py
@@ -0,0 +1,19 @@
+import influxdb_client
+
+bucket = "adsb"
+url="http://127.0.0.1:8086"
+
+client = influxdb_client.InfluxDBClient(
+    url=url
+)
+
+query_api = client.query_api()
+query = 'from(bucket:"adsb")\
+|> range(start: -5m)'
+result = query_api.query(query=query)
+results = []
+for table in result:
+    for record in table.records:
+        results.append((record.get_field(), record.get_value()))
+
+print(results)
